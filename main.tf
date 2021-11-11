308d651e4c9135209a1421305f0dd61029da91a8
mpardej <mpardej@gmail.com>
2021-11-11 17:56:51 +0100

    adding TF files

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+    version = "2.5.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "terraform_test" {
+    name = "tf_rg"
+    location =  "germanywestcentral"
+}
+
+resource "azurerm_container_group" "tf_ct_grp" {
+    name            = "wheather_grp"
+    location        = azurerm_resource_group.terraform_test.location
+    resource_group_name = azurerm_resource_group.terraform_test.name
+
+    ip_address_type     = "public"
+    dns_name_label      = "wheatherapitf"
+    os_type             = "Linux"  
+
+    container {
+        name    = "wheather"
+        image   = "mpardej/wheather"
+        cpu     = "1"
+        memory  = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        } 
+    }
+}
